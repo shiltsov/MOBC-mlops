ca2ff5b208d2928bbe998a500333d8784b03afbc
Dmitry <sda@asperito.ru>
2023-12-25 03:08:58 +0300

    deleted many files

--- mobc-mlops/infer.py
+++ mobc-mlops/infer.py
@@ -1,14 +1,11 @@
+import logging
+
 import fire
 import hydra
 import numpy as np
 import tensorflow as tf
 from dvc.api import DVCFileSystem
 from omegaconf import DictConfig
-
-
-# Data input shape
-input_shape = (28, 28, 1)
-num_classes = 10
 
 
 @hydra.main(config_path="../configs", config_name="mobc-mlops", version_base="1.3")
@@ -20,15 +17,16 @@ def infer(cfg: DictConfig = None) -> None:
     data_mean = 0.1307
     data_std = 0.3081
 
-    # Load the MNIST dataset
-
-    # y_test = np.load(y_test_name)
+    input_shape = (
+        cfg.data.input_shape_x,
+        cfg.data.input_shape_y,
+        cfg.data.input_shape_z,
+    )
 
     x_test = x_test.reshape(x_test.shape[0], x_test.shape[1], x_test.shape[2], 1)
 
     # Normalize the data
     x_test = (x_test / 255.0 - data_mean) / data_std
-    # y_test = tf.one_hot(y_test.astype(np.int32), depth=num_classes)
 
     model = tf.keras.models.Sequential(
         [
@@ -48,7 +46,7 @@ def infer(cfg: DictConfig = None) -> None:
             tf.keras.layers.Flatten(),
             tf.keras.layers.Dense(128, activation="relu"),
             tf.keras.layers.Dropout(0.5),
-            tf.keras.layers.Dense(num_classes, activation="softmax"),
+            tf.keras.layers.Dense(cfg.data.num_classes, activation="softmax"),
         ]
     )
 
@@ -56,6 +54,7 @@ def infer(cfg: DictConfig = None) -> None:
     pred = model.predict(x_test)
 
     print(pred)
+    logging.info("Done")
 
 
 if __name__ == "__main__":
--- mobc-mlops/train.py
+++ mobc-mlops/train.py
@@ -1,5 +1,10 @@
+import logging
+
 import fire
+import git
 import hydra
+import mlflow
+import mlflow.keras
 import numpy as np
 import tensorflow as tf
 from dvc.api import DVCFileSystem
@@ -25,68 +30,100 @@ def train(cfg: DictConfig = None) -> None:
     x_train = x_train.reshape(x_train.shape[0], x_train.shape[1], x_train.shape[2], 1)
     x_train = (x_train / 255.0 - data_mean) / data_std
 
-    y_train = tf.one_hot(y_train.astype(np.int32), depth=cfg["data"]["num_classes"])
+    x_test = x_test.reshape(x_test.shape[0], x_test.shape[1], x_test.shape[2], 1)
+    x_test = (x_test / 255.0 - data_mean) / data_std
+
+    y_train = tf.one_hot(y_train.astype(np.int32), depth=cfg.data.num_classes)
+    y_test = tf.one_hot(y_test.astype(np.int32), depth=cfg.data.num_classes)
 
     shape = (
-        cfg["data"]["input_shape_x"],
-        cfg["data"]["input_shape_y"],
-        cfg["data"]["input_shape_z"],
+        cfg.data.input_shape_x,
+        cfg.data.input_shape_y,
+        cfg.data.input_shape_z,
     )
 
-    # Define the architecture of the neural network
-    model = tf.keras.models.Sequential(
-        [
-            tf.keras.layers.Conv2D(
-                32,
-                (3, 3),
-                strides=(1, 1),
-                padding="valid",
-                activation="relu",
-                input_shape=shape,
-            ),
-            tf.keras.layers.Conv2D(
-                64, (3, 3), strides=(1, 1), padding="valid", activation="relu"
-            ),
-            tf.keras.layers.MaxPool2D(),
-            tf.keras.layers.Dropout(0.25),
-            tf.keras.layers.Flatten(),
-            tf.keras.layers.Dense(128, activation="relu"),
-            tf.keras.layers.Dropout(0.5),
-            tf.keras.layers.Dense(cfg["data"]["num_classes"], activation="softmax"),
-        ]
-    )
+    mlflow.set_tracking_uri(uri=cfg.mlflow.uri)
 
-    # Decay the learning rate at a base rate of gamma roughly every epoch, which
-    # is len(x_train) steps
-    scheduler = tf.keras.optimizers.schedules.ExponentialDecay(
-        cfg["training"]["learning_rate"],
-        decay_steps=len(x_train),
-        decay_rate=cfg["training"]["gamma"],
-    )
+    with mlflow.start_run():
+        # логируем коммит
+        hexsha = git.Repo(search_parent_directories=True).head.object.hexsha
+        mlflow.log_param("commit_id", hexsha)
 
-    # Define the optimizer to user for gradient descent
-    optimizer = tf.keras.optimizers.Adadelta(scheduler)
+        # логируем параметры
+        data_params = {f"data_{param}": value for param, value in cfg.data.items()}
+        model_parans = {f"model_{param}": value for param, value in cfg.model.items()}
+        training_params = {
+            f"train_{param}": value for param, value in cfg.training.items()
+        }
 
-    # Compile the model
-    model.compile(optimizer=optimizer, loss="categorical_crossentropy", metrics=["acc"])
+        mlflow.log_params(data_params)
+        mlflow.log_params(model_parans)
+        mlflow.log_params(training_params)
+        logging.info("Parameters logged")
 
-    # Display a model summary
-    print(model.summary())
+        mlflow.keras.autolog()
 
-    # Train the model
-    model.fit(
-        x_train,
-        y_train,
-        batch_size=cfg["training"]["batch_size"],
-        epochs=cfg["training"]["n_epochs"],
-        validation_data=(x_test, y_test),
-        validation_batch_size=cfg["training"]["test_batch_size"],
-    )
+        model = tf.keras.models.Sequential(
+            [
+                tf.keras.layers.Conv2D(
+                    32,
+                    (3, 3),
+                    strides=(1, 1),
+                    padding="valid",
+                    activation="relu",
+                    input_shape=shape,
+                ),
+                tf.keras.layers.Conv2D(
+                    64, (3, 3), strides=(1, 1), padding="valid", activation="relu"
+                ),
+                tf.keras.layers.MaxPool2D(),
+                tf.keras.layers.Dropout(0.25),
+                tf.keras.layers.Flatten(),
+                tf.keras.layers.Dense(128, activation="relu"),
+                tf.keras.layers.Dropout(0.5),
+                tf.keras.layers.Dense(cfg.data.num_classes, activation="softmax"),
+            ]
+        )
 
-    if cfg["training"]["save_model"]:
-        model.save_weights("../models/mnist_cnn_tf.ckpt")
+        # Decay the learning rate at a base rate of gamma roughly every epoch, which
+        # is len(x_train) steps
+        scheduler = tf.keras.optimizers.schedules.ExponentialDecay(
+            cfg.training.learning_rate,
+            decay_steps=len(x_train),
+            decay_rate=cfg.training.gamma,
+        )
 
-    print("Model ready")
+        # Define the optimizer to user for gradient descent
+        optimizer = tf.keras.optimizers.Adadelta(scheduler)
+
+        # Compile the model
+        model.compile(
+            optimizer=optimizer,
+            loss="categorical_crossentropy",
+            metrics=[
+                tf.keras.metrics.Accuracy(),
+                tf.keras.metrics.Precision(),
+                tf.keras.metrics.Recall(),
+            ],
+        )
+
+        # Display a model summary
+        logging.info(model.summary())
+
+        # Train the model
+        model.fit(
+            x_train,
+            y_train,
+            batch_size=cfg.training.batch_size,
+            epochs=cfg.training.n_epochs,
+            validation_data=(x_test, y_test),
+            validation_batch_size=cfg.training.test_batch_size,
+        )
+
+        if cfg.training.save_model:
+            model.save_weights("../models/mnist_cnn_tf.ckpt")
+
+        logging.info("Model ready")
 
 
 if __name__ == "__main__":
